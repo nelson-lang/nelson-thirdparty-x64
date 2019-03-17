1b68feea0ad74f230f9eda8250e4b65e27c7d113
Allan CORNET <nelson.numerical.computation@gmail.com>
2019-03-17 10:29:42 +0100

    boost 1.69

--- boost/update_files.bat
+++ boost/update_files.bat
@@ -1,6 +1,6 @@
 set VC_VERSION=vc141
-set BOOST_VERSION=x64-1_68
-set SRC=D:\Developpements\boost\x64\lib64-msvc-14.1
+set BOOST_VERSION=x64-1_69
+set SRC=D:\Developpements\boost\boost_1_69_0_x64\lib64-msvc-14.1
 set DST=D:\Developpements\github\nelson-thirdparty-x64\boost\lib
 mkdir %DST%
 copy %SRC%\boost_chrono-%VC_VERSION%-mt-%BOOST_VERSION%.dll %DST%
